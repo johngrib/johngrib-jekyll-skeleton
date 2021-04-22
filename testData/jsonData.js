ddecebdfc8973015a257d72f3af34128f8193350
yujonglee <yujonglee@g.skku.edu>
2021-04-22 22:20:03 +0900

    불필요한 파일 삭제

--- testData/jsonData.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs'
-
-export default function getJSONTestData(name) {
-    return fs.readFileSync(`testData/fixture/tag/${name}.json`).toString();
-}
--- testData/yamlData.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs'
-
-export default function getYAMLTestData(name) {
-    return fs.readFileSync(`testData/fixture/yaml/${name}.yml`).toString();
-}
