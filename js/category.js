e10f62ab0e4a60ccdf0e0be2b2251496a793884e
John Grib <johngrib82@gmail.com>
2021-07-10 12:25:13 +0900

    category.js 파일에 주석을 달아라

--- js/category.js
+++ js/category.js
@@ -4,6 +4,9 @@
         return thisName;
     }
 
+    /**
+     * 자식 문서들의 데이터를 받아, 자식 문서들의 데이터 list로 리턴합니다.
+     */
     function getChildren(data) {
         var thisName = getTarget();
         var list = [];
@@ -25,7 +28,22 @@
         return list;
     }
 
-    function getChildrenHTML(list) {
+    /**
+     * 자식 문서들의 list를 받아, 링크를 만들어 html 문자열로 리턴합니다.
+     */
+    function makeCategoryChildrenLinks(list) {
+            /* 생성된 html 문자열 예
+            <li>
+                <a href="/wiki/bash-history" class="post-link">
+                    <span>bash history 다루기</span>
+                    <div class="post-meta" style="float: right;">2019-12-21</div>
+                    <div class="post-excerpt"> - 사용법 및 잡다한 팁 요약</div>
+                </a>
+            </li>
+            <li>
+                ...
+            </li>
+             */
         var children = '';
         for (var i = 0; i < list.length; i++) {
             var url = list[i].url;
@@ -37,6 +55,9 @@
         return children;
     }
 
+    /*
+     * category 타입의 문서 내부에 하위 문서 목록을 만들어 줍니다.
+     */
     axios
         .get('/data/wikilist.json', {})
         .then(function(resp) {
@@ -44,7 +65,7 @@
                 return;
             }
             var list = getChildren(resp.data);
-            var html = getChildrenHTML(list);
+            var html = makeCategoryChildrenLinks(list);
             document.getElementById('document-list').innerHTML = `<ul class="post-list">${html}</ul>`
 
             return;
